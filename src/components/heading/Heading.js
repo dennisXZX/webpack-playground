d137975e57b3308ae1d4840a98bf00b41a59e9e3
Dennis Xiao <dennisboys@gmail.com>
2019-06-04 21:25:29 +1000

    add heading component

---
+++ src/components/heading/Heading.js
@@ -0,0 +1,16 @@
+import './heading.scss'
+
+class Heading {
+  render() {
+    const h1 = document.createElement('h1')
+    const body = document.querySelector('body')
+
+    h1.innerHTML = 'Webpack is awesome'
+
+    h1.classList.add('heading')
+
+    body.appendChild(h1)
+  }
+}
+
+export default Heading
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import HelloWorldButton from './components/hello-world-button/hello-world-button'
+import HelloWorldButton from './components/hello-world-button/Hello-world-button'
+import Heading from './components/heading/Heading'
+
 import addImage from './utils/add-image'
 
 addImage()
 
 const helloWorldButton = new HelloWorldButton()
-helloWorldButton.render();
+helloWorldButton.render()
+
+const heading = new Heading()
+heading.render()
